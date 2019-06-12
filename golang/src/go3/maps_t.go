d943a663ded310a027a570e1cd6df67d363f372b
sun.xin <sun.xin@linecorp.com>
2019-06-12 16:57:26 +0800

    003

---
+++ golang/src/go3/maps_t.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+func WordCount(s string) map[string]int {
+	return nil
+}
+
+func main() {
+	fmt.Println(WordCount("I am learning Go!"))
+	fmt.Println(WordCount("I ate a donut. Then I ate another donut."))
+}
